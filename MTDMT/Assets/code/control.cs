d4728ab610fbc4cb70ddecbe41f2652fa527b0e7
xztumzx <164007038+xztumzx@users.noreply.github.com>
2024-06-17 22:31:07 +0700

    animation s and w

--- MTDMT/Assets/code/control.cs
+++ MTDMT/Assets/code/control.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class control : MonoBehaviour
 {
     public Animator anim;
+    public CharacterController characontroll;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        characontroll = GetComponent<CharacterController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("s"))
+        if (Input.GetKey(KeyCode.S))
         {
-            //print("©Ñ¹¡´1");
-
-            anim.Play("walk-s", -1, 0f);
+            anim.SetBool("Moving s", true);
         }
+        else { anim.SetBool("Moving s", false); }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("Moving w", true);
+        }
+        else { anim.SetBool("Moving w", false); }
     }
 }
