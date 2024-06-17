8014a8d137892ebedaf63d7f7af74b8f97086c97
2oeyt <naphatnoey.taichan@gmail.com>
2024-06-17 23:24:52 +0700

    testNextScene
    corgi

---
+++ MTDMT/Assets/imagNoeyTest/SceneScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void onClickNextScene() {
+        SceneManager.LoadScene("noey");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
