cf442faf836064aa3a440a2f1428eb9c42a6b716
2oeyt <naphatnoey.taichan@gmail.com>
2024-06-18 23:13:57 +0700

    hp bar
    damage and health

---
+++ MTDMT/Assets/code/HpBar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBar : MonoBehaviour
+{
+    public Image hpFill;
+    public float maxHp;
+    public float currentHp; // (เลือดไม่เริ่มใหม่)
+
+    void Start()
+    {
+        // currentHp = maxHp; //เลือดรีใหม่
+    }
+    void takeDamage(float damage)
+    {
+        currentHp -= damage;
+
+        hpFill.fillAmount = currentHp * (1 / maxHp);
+    }
+
+    void takeHeal(float Heal)
+    {
+        currentHp += Heal;
+
+        hpFill.fillAmount = currentHp * (1 / maxHp);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            takeDamage(10);
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            takeHeal(20);
+        }
+    }
+}
