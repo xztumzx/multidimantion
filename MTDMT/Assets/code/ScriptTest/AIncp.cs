09eff10c2fbc19011e140576fad010ddea04e62f
2oeyt <naphatnoey.taichan@gmail.com>
2024-06-23 17:14:08 +0700

    test commit

---
+++ MTDMT/Assets/code/ScriptTest/AIncp.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIncp : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("ChangeDestination", 1f, 1f);
+    }
+
+    void ChangeDestination()
+    {
+        agent.SetDestination(target.transform.position);
+    }
+
+}
