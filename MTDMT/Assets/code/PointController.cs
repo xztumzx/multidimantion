1a36bb2596cf4af60ff4c3f716c4bf2aa8774df2
2oeyt <naphatnoey.taichan@gmail.com>
2024-06-26 21:08:08 +0700

    test

--- MTDMT/Assets/code/PointController.cs
+++ MTDMT/Assets/code/PointController.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class SceneScript : MonoBehaviour
+public class PointController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void onClickNextScene() {
-        SceneManager.LoadScene("noey3");
+    void Start()
+    {
         
     }
 
