a5079751d2eab129fbd6952adab37efb0244274c
2oeyt <naphatnoey.taichan@gmail.com>
2024-07-02 23:05:28 +0700

    stamina 90%

--- MTDMT/Assets/code/Player/fadel/dash.cs
+++ MTDMT/Assets/code/Player/fadel/dash.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class dash : MonoBehaviour
 {
-    public float dashSpeed = 20f;       // ¤ÇÒÁàÃçÇã¹¡ÒÃ dash
-    public float dashDuration = 0.2f;   // ÃÐÂÐàÇÅÒã¹¡ÒÃ dash
-    public KeyCode dashKey = KeyCode.LeftShift; // »ØèÁ·Õèãªéã¹¡ÒÃ dash
+    public float dashSpeed = 20f;
+    public float dashDuration = 0.2f;
+    public KeyCode dashKey = KeyCode.LeftShift;
 
     private Rigidbody rb;
     private bool isDashing = false;
     private float dashTime;
     private Vector3 dashDirection;
+    private PlayerStats playerStats;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        playerStats = FindObjectOfType<PlayerStats>();
     }
 
     void Update()
@@ -33,10 +35,13 @@ public class dash : MonoBehaviour
 
     void StartDash()
     {
-        isDashing = true;
-        dashTime = Time.time + dashDuration;
-        dashDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
-        rb.velocity = dashDirection * dashSpeed;
+        if (playerStats != null && playerStats.CanDash())
+        {
+            isDashing = true;
+            dashTime = Time.time + dashDuration;
+            dashDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+            rb.velocity = dashDirection * dashSpeed;
+        }
     }
 
     void ContinueDash()
@@ -50,6 +55,6 @@ public class dash : MonoBehaviour
     void StopDash()
     {
         isDashing = false;
-        rb.velocity = Vector3.zero;  // ËÂØ´¡ÒÃà¤Å×èÍ¹·Õè
+        rb.velocity = Vector3.zero;
     }
 }
--- MTDMT/Assets/code/UI/Menu/MenuScript.cs
+++ MTDMT/Assets/code/UI/Menu/MenuScript.cs
@@ -14,11 +14,67 @@ public class MenuScript : MonoBehaviour
 
     [SerializeField] private GameObject comfirmationPrompt = null;*/
 
+    /*[Header("Gameplay Settings")]
+    [SerializeField] private TMP_Text controllerSenTextValue = null;
+    [SerializeField] private Slider controllerSenSlider = null;
+    [SerializeField] private int defaultSen = 4; // can change this up to you***
+    public int mainControllerSen = 4;*/
+
+    [Header("Graphics Setting")]
+    [SerializeField] private Slider brightnessSlider = null;
+    [SerializeField] private TMP_Text brightnessTextValue = null;
+    [SerializeField] private float defaultBrightness = 1;
+
+    [Space(10)]
+    [SerializeField] private TMP_Dropdown qualityDropdown;
+    [SerializeField] private Toggle fullScreenToggle;
+
+    private int _qualityLevel;
+    private bool _isFullScreen;
+    private float _brightnessLevel;
+
+    [Header ("Confirmation")]
+    [SerializeField] private GameObject comfirmationPrompt = null;
 
     [Header("Levels To Load")]
     //public string _newGameStory;
     private string storyToLoad;
     [SerializeField] private GameObject noSaveGameDialog = null;
+
+    /*[Header("Resolution Dropdown")]
+    public TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;*/
+
+    private void Start()
+    {
+        /*resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();*/
+    }
+
+    /*public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }*/
 
     public void NewGameDialogYes()
     {
@@ -44,6 +100,58 @@ public class MenuScript : MonoBehaviour
         Application.Quit();
     }
 
+    /*public void SetControllerSen(float sensitivity)
+    {
+        mainControllerSen = Mathf.RoundToInt(sensitivity);
+        controllerSenTextValue.text = sensitivity.ToString("0");
+    }*/
+
+    public void GameplayApply()
+    {
+
+
+        /*if (invertYToggle.isOn)
+        {
+            PlayerPrefs.SetInt("masterInvertaY", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("masterInvertY", 0);
+        }
+
+        PlayerPrefs.SetInt("masterSen")*/
+    }
+
+    public void SetBrightness(float brightness)
+    {
+        _brightnessLevel = brightness;
+        brightnessTextValue.text = brightness.ToString("0.0");
+    }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        _isFullScreen = isFullscreen;
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        _qualityLevel = qualityIndex;
+    }
+
+    public void GraphicApply()
+    {
+        PlayerPrefs.SetFloat("masterBrightness", _brightnessLevel);
+        // Change your brightness with your post processing or whatever it is
+
+        PlayerPrefs.SetInt("masterQuality", _qualityLevel);
+        QualitySettings.SetQualityLevel(_qualityLevel);
+
+        PlayerPrefs.SetInt("masterFullscreen", (_isFullScreen ? 1 : 0)); 
+        Screen.fullScreen = _isFullScreen;
+
+        StartCoroutine(ConfirmationBox());
+    }
+
     /*public void SetVolume(float volume)
     {
         AudioListener.volume = volume;
@@ -54,16 +162,26 @@ public class MenuScript : MonoBehaviour
     {
         PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
         StartCoroutine(ConfirmationBox()); 
-    }
+    }*/
 
     public void ResetButton(string MenuType)
     {
-        if (MenuType == "Audio")
+        if (MenuType == "Graphics")
         {
-            AudioListener.volume = defaultVolume;
-            volumeSlider.value = defaultVolume;
-            volumeTeextValue.text = defaultVolume.ToString("0.0");
-            VolumeApple();
+            // Reset Brightness Value
+            brightnessSlider.value = defaultBrightness;
+            brightnessTextValue.text = defaultBrightness.ToString("0.0");
+
+            qualityDropdown.value = 1;
+            QualitySettings.SetQualityLevel(1);
+
+            fullScreenToggle.isOn = false;
+            Screen.fullScreen = false;
+
+            /*Resolution currentResolution = Screen.currentResolution;
+            Screen.SetResolution(currentResolution.width, currentResolution.height, Screen.fullScreen);
+            resolutionDropdown.value = resolutions.Length;*/
+            GraphicApply();
         }
     }
 
@@ -72,6 +190,6 @@ public class MenuScript : MonoBehaviour
         comfirmationPrompt.SetActive(true);
         yield return new WaitForSeconds(2);
         comfirmationPrompt.SetActive(false);
-    }*/
+    }
 
 }
