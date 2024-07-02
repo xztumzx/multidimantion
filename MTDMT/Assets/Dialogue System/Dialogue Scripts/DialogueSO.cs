1c2c49cbf11444100ee78324ea8344c040064b94
Neatprapa <nneatpream@gmail.com>
2024-07-02 23:33:19 +0700

    หวั่น! สาวคอมดับงานเกือบหาย

--- MTDMT/Assets/Dialogue System/Dialogue Scripts/DialogueSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Dialogue", menuName = "Tutorial/Dialogue")]
-public class DialogueSO : ScriptableObject
-{
-
-}
