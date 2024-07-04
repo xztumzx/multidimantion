c64154f2a29824bc039aa5e551118f4568f21361
2oeyt <naphatnoey.taichan@gmail.com>
2024-07-04 23:56:36 +0700

    ***********ERROR ENEMY***********
    not have animator

--- MTDMT/Assets/code/Enemy/EnemyLocomotionManager.cs
+++ MTDMT/Assets/code/Enemy/EnemyLocomotionManager.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.TextCore.Text;
 
 public class EnemyLocomotionManager : MonoBehaviour
 {
     EnemyManager enemyManager;
+    EnemyAnimatorManager enemyAnimatorManager;
+    NavMeshAgent navmeshAgent;
+    public Rigidbody enemyRigidBody;
 
     public CharacterStats currentTarget;
     public LayerMask detectionLayer;
 
+    public float distanceFromTarget;
+    public float stoppingDistance = 1f;
+
+    public float rotationSpeed = 15;
+
     private void Awake()
     {
         enemyManager = GetComponent<EnemyManager>();
+        enemyAnimatorManager = GetComponentInChildren<EnemyAnimatorManager>();
+        navmeshAgent = GetComponentInChildren<NavMeshAgent>();
+        enemyRigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        navmeshAgent.enabled = false;
+        enemyRigidBody.isKinematic = false;
     }
     public void HandleDetaction()
     {
@@ -34,6 +52,76 @@ public class EnemyLocomotionManager : MonoBehaviour
                     currentTarget  = characterState;
                 }
             }
+        }
+    }
+
+    public void HandleMoveToTarget()
+    {
+        if (enemyAnimatorManager == null)
+        {
+            Debug.LogError("enemyAnimatorManager is not assigned!");
+            return;
+        }
+
+        if (enemyAnimatorManager.anim == null || enemyAnimatorManager.anim.runtimeAnimatorController == null)
+        {
+            Debug.LogError("Animator or AnimatorController is not properly assigned!");
+            return;
+        }
+
+        Vector3 targetDirection = currentTarget.transform.position - transform.position;
+        distanceFromTarget = Vector3.Distance(currentTarget.transform.position, transform.position);
+        float viewableAngle = Vector3.Angle(targetDirection, transform.forward);
+
+        //If we are preforming an action, stop our movement!
+        if (enemyManager.isPreformingAction)
+        {
+            enemyAnimatorManager.anim.SetFloat("Vertical", 0, 0.1f, Time.deltaTime);
+            navmeshAgent.enabled = false;
+        }
+        else
+        {
+            if (distanceFromTarget > stoppingDistance)
+            {
+                enemyAnimatorManager.anim.SetFloat("Vertical", 1, 0.1f, Time.deltaTime);
+            }
+            else if (distanceFromTarget <= stoppingDistance)
+            {
+                enemyAnimatorManager.anim.SetFloat("Vertical", 0, 0.1f, Time.deltaTime);
+            }
+        }
+
+        HandleRotateTowardsTarget();
+        navmeshAgent.transform.localPosition = Vector3.zero;
+        navmeshAgent.transform.localRotation = Quaternion.identity;
+    }
+
+    private void HandleRotateTowardsTarget()
+    {
+        // Rotate manually
+        if (enemyManager.isPreformingAction)
+        {
+            Vector3 direction = currentTarget.transform.position - transform.position;
+            direction.y = 0;
+            direction.Normalize();
+
+            if (direction == Vector3.zero)
+            {
+                direction = transform.forward;
+            }
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed / Time.deltaTime);
+        }
+        // Rotate with pathfinding (navmesh)
+        else
+        {
+            Vector3 relativeDirection = transform.InverseTransformDirection(navmeshAgent.desiredVelocity);
+            Vector3 targetVelocity = enemyRigidBody.velocity;
+
+            navmeshAgent.enabled = true;
+            navmeshAgent.SetDestination(currentTarget.transform.position);
+            enemyRigidBody.velocity = targetVelocity;
+            transform.rotation = Quaternion.Slerp(transform.rotation, navmeshAgent.transform.rotation, rotationSpeed / Time.deltaTime);
         }
     }
 
