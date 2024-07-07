700fbe57437944ae31dc8c62a0ee81a6390f4827
Neatprapa <nneatpream@gmail.com>
2024-07-07 13:58:10 +0700

    asdfgioเสร็จแร้ว แส้วเหล็ด สักวาหน้าหนาวสาวๆขาแตก แตกเป็นรอยแยก

--- MTDMT/Assets/HeneGames/Dialogue System/Scripts/DialogueTrigger.cs
+++ MTDMT/Assets/HeneGames/Dialogue System/Scripts/DialogueTrigger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-public class Script : MonoBehaviour
+public class DialogueTrigger : MonoBehaviour
 {
     [Header("Events")]
     public UnityEvent startDialogueEvent;
